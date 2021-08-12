6b26a5721b345d6299b476c10baf88028300f136
Aayush Chaube <chaubeaayush@gmail.com>
2021-08-12 16:33:31 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/aayush/testpackages/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aayush.testpackages
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const MyStatefullWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefullWidget extends StatefulWidget {
+  const MyStatefullWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefullWidgetState createState() => _MyStatefullWidgetState();
+}
+
+class _MyStatefullWidgetState extends State<MyStatefullWidget> {
+  double progress = 0;
+  var speed = 0;
+
+  void calculateProgress(int received, int total) {
+    var result = (received / total) * 100;
+    setState(() {
+      progress = result;
+      print('Progress:  $progress');
+    });
+  }
+
+  // void calculateSpeed(int received) {
+  //   Timer(
+  //     const Duration(seconds: 1),
+  //     () {
+  //       speed = received - diffByte;
+  //       var diffByte = received;
+  //     },
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Dummy PDF'),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text('PDF'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                Response response;
+                Dio dio = Dio();
+
+                response = await dio.download(
+                  'https://research.nhm.org/pdfs/10840/10840.pdf',
+                  'D:/dummy.pdf',
+                  onReceiveProgress: (current, total) {
+                    calculateProgress(current, total);
+                    // calculateSpeed(current);
+                  },
+                  deleteOnError: true,
+                );
+              },
+              child: const Text('DOWNLOAD'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text('Progress:   ${progress.round().toString()}%'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: LinearProgressIndicator(
+              minHeight: 30.0,
+              value: progress / 100,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child:
+                Text('Download Speed:   ${speed.toStringAsFixed(2)} bytes/s'),
+          ),
+        ],
+      ),
+    );
+  }
+}
